2a0c251d2e4ca393415ef70071f7cba55518dfb0
Davy <kineese@googlemail.com>
2020-12-13 20:42:00 +0100

    Storing colshapes and removing on player disconnect

--- PlainServer/Main.cs
+++ PlainServer/Main.cs
@@ -4,6 +4,7 @@ using AltV.Net.Elements.Entities;
 using AltV.Net.Enums;
 using NLog;
 using System;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace PlainServer
     public class Main : AsyncResource
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private static ConcurrentStack<IColShape> ColShapes = new ConcurrentStack<IColShape>();
         public Main()
         { }
         public override void OnStart()
@@ -24,32 +26,47 @@ namespace PlainServer
             LogManager.Configuration = cfg;
             AltAsync.OnColShape += HandleColshape;
             AltAsync.OnPlayerConnect += HandlePlayerConnect;
+            AltAsync.OnPlayerDisconnect += HandlePlayerDisconnect;
+
+        }
+
+        private async Task HandlePlayerDisconnect(IPlayer player, string reason)
+        {
+            Logger.Info("Player disconnected. Removing colshape");
+            ColShapes.TryPop(out var colShape);
+            await AltAsync.Do(() =>
+            {
+                Alt.RemoveColShape(colShape);
+            });
         }
 
         private async Task HandleColshape(IColShape colShape, IEntity targetEntity, bool state)
         {
-            switch (targetEntity)
+            lock (targetEntity)
             {
-                case IPlayer player when state:
-                    {
-                        Logger.Info("Player entered a colshape");
-                        break;
-                    }
-                case IPlayer player:
-                    {
-                        Logger.Info("Player left a colshape");
-                        break;
-                    }
-                case IVehicle vehicle when state:
-                    {
-                        Logger.Info("Vehicle entered a colshape");
-                        break;
-                    }
-                case IVehicle vehicle:
-                    {
-                        Logger.Info("Vehicle left a colshape");
-                        break;
-                    }
+                switch (targetEntity)
+                {
+                    case IPlayer player when state:
+                        {
+                            Logger.Info("Player entered a colshape");
+                            break;
+                        }
+                    case IPlayer player:
+                        {
+                            Logger.Info("Player left a colshape");
+                            break;
+                        }
+                    case IVehicle vehicle when state:
+                        {
+                            Logger.Info("Vehicle entered a colshape");
+                            break;
+                        }
+                    case IVehicle vehicle:
+                        {
+                            Logger.Info("Vehicle left a colshape");
+                            break;
+                        }
+                }
             }
         }
 
@@ -60,12 +77,14 @@ namespace PlainServer
                 Logger.Info($"Player {player.Name} connected. Creating Colshape");
                 player.Model = (uint)PedModel.FreemodeMale01;
                 player.Spawn(new AltV.Net.Data.Position(813, -279, 66), 1000);
-                AltAsync.Do(() =>
-                {
-                    Alt.CreateColShapeSphere(new AltV.Net.Data.Position(813, -279, 66), 10);
-                });
-                Logger.Info("Created Colshape");
             }
+
+            await AltAsync.Do(() =>
+            {
+                var colshapeToAdd = Alt.CreateColShapeSphere(new AltV.Net.Data.Position(813, -279, 66), 10);
+                ColShapes.Push(colshapeToAdd);
+            });
+            Logger.Info("Created Colshape");
         }
 
         private async Task HandleClientEvent(IPlayer player, string eventName, object[] args)
